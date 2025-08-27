cd40b2f6eabbf1a72713f1c3806a6111f08cf60b
Orvil Alejandro Ibarra Patiño <142275946+Xiclez@users.noreply.github.com>
2025-08-26 19:48:09 -0600

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1756259290848

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c8596582._.js",
+  "static/chunks/node_modules_f7c794ac._.js"
+],
+    source: "dynamic"
+});
